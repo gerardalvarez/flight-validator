e9ca3ce4f12f90c1d0aa78cd1b88ac34c331dd93
Gerard <gerardalvariz@gmail.com>
2023-10-04 20:04:26 +0200

    Initial commit, started the repository

---
+++ src/main/java/com/nuvolartest/flightvalidator/FlightValidatorApplication.java
@@ -0,0 +1,14 @@
+package com.nuvolartest.flightvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightValidatorApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FlightValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nuvolartest/flightvalidator/FlightValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuvolartest.flightvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
